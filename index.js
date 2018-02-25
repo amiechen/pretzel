64802cbac35456f985850eaaa06c40d4446ee413
Amie Chen <chinghc@gmail.com>
2018-02-24 22:22:32 -0800

    add tray

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain, Tray} = require('electron')
 const path = require('path')
 const url = require('url')
+const assetsDirectory = path.join(__dirname, 'assets')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// global references
+let tray
 let win
 
+function toggleWindow () {
+  if (win.isVisible()) {
+    win.hide()
+  } else {
+    win.show()
+    win.focus()
+  }
+}
+
+function createTray () {
+  tray = new Tray(path.join(assetsDirectory, 'dot.png'))
+  tray.setTitle('hi')
+  tray.on('right-click', toggleWindow)
+  tray.on('double-click', toggleWindow)
+  tray.on('click', function (event) {
+    toggleWindow()
+    win.openDevTools({mode: 'detach'})
+
+    // Show devtools when command clicked
+    // if (win.isVisible() && process.defaultApp && event.metaKey) {
+    // }
+  })
+}
+
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
+  win.on('blur', () => {
+    if (!win.webContents.isDevToolsOpened()) {
+      win.hide
+    }
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.dock.hide()
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// called when Electron has finished initialization and is ready to create browser windows.
+app.on('ready', () => {
+  createTray()
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+app.on('window-all-closed', () => {
+  app.quit()
 })

894021b6eee1304ec7f2025295c6307461b28623
Amie Chen <chinghc@gmail.com>
2018-03-07 14:45:36 -0800

    dump shortcuts from yaml files

--- renderer.js
+++ renderer.js
@@ -1,13 +1,28 @@
 const ipcRenderer = require("electron").ipcRenderer;
+const yaml = require("js-yaml");
 const _ = require("underscore");
+const fs = require("fs");
 const appName = document.querySelector(".app-name");
+const shortcutsContainer = document.querySelector(".shortcuts-container");
 
 _.templateSettings = {
   interpolate: /\{\{(.+?)\}\}/g
 };
 
+function findShortcutsByName(name) {
+  try {
+    const config = yaml.safeLoad(
+      fs.readFileSync(`shortcuts/${name}.yml`, "utf8")
+    );
+    const indentedJson = JSON.stringify(config, null, 4);
+    return indentedJson;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 ipcRenderer.on("currentApp", (event, name) => {
-  // const shortcuts = findShortcutsByName(name)
-  const template = _.template("Hello {{ name }}!");
-  appName.innerHTML = template({ name: name });
+  const appNameTemplate = _.template("Hello {{ name }}!");
+  appName.innerHTML = appNameTemplate({ name: name });
+  shortcutsContainer.innerHTML = findShortcutsByName(name);
 });

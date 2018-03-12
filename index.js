6c8ae7c1ba224d8316519db9b2471a2e149bf773
Amie Chen <chinghc@gmail.com>
2018-03-11 20:26:36 -0700

    gray out icon when no shortcuts

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ const objc = require("objc");
 const assetsDirectory = path.join(__dirname, "assets");
 const shortcutsDirectory = path.join(__dirname, "shortcuts");
 const menubar = require("menubar");
+let previousApp;
 let mb = menubar({
-  dir: __dirname,
-  index: path.join("file://", __dirname, "index.html"),
-  icon: path.join(assetsDirectory, "shortcuts@2x.png"),
-  tooltip: "Shortcuts",
-  width: 1000,
-  height: 500,
-  showDockIcon: false,
-  resizable: false,
-  preloadWindow: true
-});
+    dir: __dirname,
+    index: path.join("file://", __dirname, "index.html"),
+    icon: path.join(assetsDirectory, "shortcuts-gray@2x.png"),
+    tooltip: "Shortcuts",
+    width: 1000,
+    height: 500,
+    showDockIcon: false,
+    resizable: false,
+    preloadWindow: true
+  });
 
 objc.import("AppKit");
 const { NSWorkspace, js } = objc;
+
+function activateWindow(currentApp) {
+  mb.tray.setImage(path.join(assetsDirectory, "shortcuts@2x.png"));
+  previousApp = currentApp;
+}
+
+function deactivateWindow(currentApp) {
+  mb.tray.setImage(path.join(assetsDirectory, "shortcuts-gray@2x.png"));
+  previousApp = currentApp;
+}
 
 mb.on("show", () => {
   let currentAppProxy = NSWorkspace.sharedWorkspace()
@@ -41,7 +52,7 @@ mb.on("show", () => {
 });
 
 mb.on("after-create-window", () => {
-  mb.window.openDevTools();
+  //mb.window.openDevTools();
 });
 
 // return window focus back to the previous app, like the behavior on mac app
@@ -49,8 +60,22 @@ mb.on("after-hide", () => {
   Menu.sendActionToFirstResponder("hide:");
 });
 
-// display window on shortcut
 app.on("ready", () => {
+  const availableShortcuts = fs.readdirSync(shortcutsDirectory);
+
+  // do pulling every second
+  setInterval(() => {
+    let currentAppProxy = NSWorkspace.sharedWorkspace()
+      .frontmostApplication()
+      .localizedName();
+    let currentApp = js(currentAppProxy);
+
+    if (currentApp !== previousApp) {
+      availableShortcuts.indexOf(`${currentApp}.yml`) > -1 ? activateWindow(currentApp) : deactivateWindow(currentApp);
+    }
+  }, 1000);
+
+  // display window on shortcut
   globalShortcut.register("Command+1", () => {
     mb.window.isVisible() ? mb.hideWindow() : mb.showWindow();
   });
